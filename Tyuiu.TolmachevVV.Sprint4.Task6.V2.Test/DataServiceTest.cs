b94c7410b35fd1ae990c487222c1b8e23185f53e
Vitaly Tolmachev <vitalhare@outlook.com>
2023-11-11 23:07:48 +0500

    Task6

---
+++ Tyuiu.TolmachevVV.Sprint4.Task6.V2.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.TolmachevVV.Sprint4.Task6.V2.Lib;
+
+namespace Tyuiu.TolmachevVV.Sprint4.Task6.V2.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService service1 = new DataService();
+            CollectionAssert.AreEqual(new string[] { "Черный", "Зеленый", "Красный", "Желтый", "Фиолетовый" }, service1.Calculate(new string[] { "Белый", "Черный", "Зеленый", "Синий", "Красный", "Желтый", "Фиолетовый" }));
+        }
+    }
+}
---
+++ Tyuiu.TolmachevVV.Sprint4.Task6.V2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.TolmachevVV.Sprint4.Task6.V2.Lib;
+
+namespace Tyuiu.TolmachevVV.Sprint4.Task6.V2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService service1 = new DataService();
+            Console.Title = "Спринт #4 | Выполнил: Толмачев. В. В. | ПКТб-23-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("*Спринт #4                                                                *");
+            Console.WriteLine("*Задание #6                                                               *");
+            Console.WriteLine("*Вариант #2                                                               *");
+            Console.WriteLine("*Выполнил: Толмачев Виталий Владимирович | ПКТб 23-1                      *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("Белый, Черный, Зеленый, Синий, Красный, Желтый, Фиолетовый                *");
+            string[] array = service1.Calculate(new string[] { "Белый", "Черный", "Зеленый", "Синий", "Красный", "Желтый", "Фиолетовый" });
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            foreach(string item in array)
+            {
+                Console.Write(item + "   ");
+            }
+            Console.WriteLine();
+            Console.WriteLine("***************************************************************************");
+            Console.ReadKey();
+        }
+    }
+}
